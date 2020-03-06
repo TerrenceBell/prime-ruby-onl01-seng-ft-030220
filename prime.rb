74c78fe3b117428aae4a4318d25f7ffda5ad6636
Terrence Bell <terrencekbell@gmail.com>
2020-03-06 02:55:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,12 +4,7 @@ if num <= 1
   return false
 else 
 (2..num-1).all? do |int|
-  
-
-#   num.find do |int|
-#   if prime.prime?(int)
-#     else
-#     return false
-# end
-# end
-# end
+  num.modulo(int) > 0 
+end
+end
+end
