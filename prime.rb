898e08e5ee7a739df107b6b59f4930ead970c7fc
Terrence Bell <terrencekbell@gmail.com>
2020-03-06 02:33:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
-def prime?(num)
-  num.find do |int|
-  if prime.prime?(int)
-    return true 
-  else
-    return false
-end
- end
- end
+def prime(num)
+#   num.find do |int|
+#   if prime.prime?(int)
+#     else
+#     return false
+# end
+# end
+# end
